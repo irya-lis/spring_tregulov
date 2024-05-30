4ccdf53155a3033028d97f374d20fe6f6c8a4837
Irina Rakova <lis.ovskaya1119@gmail.com>
2024-05-30 18:00:30 +0300

    dependency injection using constructor

--- src/main/com/github/irya_lis/spring_introduction/Cat.java
+++ src/main/com/github/irya_lis/spring_introduction/Cat.java
@@ -1,11 +1,10 @@
 package com.github.irya_lis.spring_introduction;
 
-public class Dog extends AbstractPet {
-
+public class Cat extends AbstractPet {
 
 
     @Override
     public void say() {
-        System.out.println(  " say " +  "Waff-waff");
+        System.out.println( " say " + "Meeo-meeeo");
     }
 }
--- src/main/com/github/irya_lis/spring_introduction/TestDog.java
+++ src/main/com/github/irya_lis/spring_introduction/TestDog.java
@@ -1,9 +1,9 @@
 package com.github.irya_lis.spring_introduction;
 
-public class Test_dog {
+public class TestDog {
     public static void main(String[] args) {
-        Pet pet = new Dog("Dana");
-        AbstractPet abstractPet = new Dog("d");
+        Pet pet = new Dog();
+        AbstractPet abstractPet = new Dog();
         pet.say();
         abstractPet.say();
     }
