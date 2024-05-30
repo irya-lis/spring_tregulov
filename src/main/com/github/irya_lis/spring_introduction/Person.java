cd0f35a760c650b723642b960c360a82d0f5e3f7
Irina Rakova <lis.ovskaya1119@gmail.com>
2024-05-30 18:30:31 +0300

    dependency injection using setter

--- src/main/com/github/irya_lis/spring_introduction/Person.java
+++ src/main/com/github/irya_lis/spring_introduction/Person.java
@@ -2,10 +2,9 @@ package com.github.irya_lis.spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
-//    public Person(Pet pet) {
-//        this.pet = pet;
-//    }
         public Person() {
             System.out.println("Person bean is created");
     }
@@ -21,4 +20,25 @@ public class Person {
         pet.say();
     }
 
+    public Person(String surname, int age) {
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+
+    public void setAge(String age) {
+    }
 }
--- src/main/com/github/irya_lis/spring_introduction/Test3.java
+++ src/main/com/github/irya_lis/spring_introduction/Test3.java
@@ -9,6 +9,11 @@ public class Test3 {
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
+
+        System.out.println(person.getSurname());
+        System.out.println( person.getAge());
+
+
         context.close();
     }
 }
