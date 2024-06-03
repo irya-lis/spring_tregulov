3280cca45b918adb5b414244ed8b62c3cdc9e054
Irina Rakova <lis.ovskaya1119@gmail.com>
2024-06-03 17:27:22 +0300

    @Component

--- src/main/com/github/irya_lis/spring_introduction/Cat.java
+++ src/main/com/github/irya_lis/spring_introduction/Cat.java
@@ -1,7 +1,14 @@
 package com.github.irya_lis.spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component("cat")
+
 public class Cat extends AbstractPet {
 
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
 
     @Override
     public void say() {
---
+++ src/main/com/github/irya_lis/spring_introduction/ConfigWithAnnotations1.java
@@ -0,0 +1,16 @@
+package com.github.irya_lis.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigWithAnnotations1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext3.xml");
+
+        Cat myCat = context.getBean("cat", Cat.class);
+
+        myCat.say();
+
+        context.close();
+    }
+}
